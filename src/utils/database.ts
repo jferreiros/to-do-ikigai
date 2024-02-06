f6f60e9a3af8fec0a44671e239cc9045a87133a0
joaquinferreiros <jferreiros@emailingnetwork.com>
2024-02-06 00:13:44 -0500

    Remove .env file

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,11 +1,20 @@
 import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const connectDB = async () => {
+  let mongoUri = process.env.MONGODB_URI;
+
+  // Check if we want to use an in-memory database
+  if (process.env.USE_MEMORY_DB === 'true') {
+    const mongoMemoryServer = await MongoMemoryServer.create();
+    mongoUri = mongoMemoryServer.getUri();
+  }
+
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(mongoUri);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
